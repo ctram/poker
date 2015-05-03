fdd92ed74458404e1c5220504aade5add0fd37a9
Chris <chris@chriss-mbp.attlocal.net>
2015-05-03 00:35:12 -0700

    Reorganize specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,13 @@
 require 'game'
 
 describe Game do
+  let(:player1){double("player1")}
+  let(:player2){double("player2")}
+  let(:player3){double("player3")}
+  let(:players){ [player1, player2, player3]}
+  let(:game){Game.new(players)}
+
   describe '#initialize' do
-    let(:player1){double("player1")}
-    let(:player2){double("player2")}
-    let(:player3){double("player3")}
-    let(:players){ [player1, player2, player3]}
-    let(:game){Game.new(players)}
-
-
     it 'starts a game with a deck of 52 cards' do
       expect(game.deck.count).to eq(52)
     end
@@ -19,12 +18,24 @@ describe Game do
 
   end
 
+  describe '#turn' do
+
+    it 'implements turns' do
+      
+    end
 
 
 
-
-    it 'implements turns'
-    it 'tracks bets made by players'
     it 'tracks the pot per turn'
+  end
+
+  describe '#take_bets' do
+    it 'tracks bets made by players'
+  end
+
+
+
+
+
 
 end
